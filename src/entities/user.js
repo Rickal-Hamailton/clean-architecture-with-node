c8328e93d831da6f8b31d8428d94a0caa233c52c
Rickal Hamilton <rhamilton@palaceresorts.com>
2023-02-20 16:32:10 -0500

    adding user entity and repository

---
+++ src/entities/user.js
@@ -0,0 +1,23 @@
+export class User {
+  constructor({
+    id,
+    name = null,
+    lastName = null,
+    gender = genders.NOT_SPECIFIED,
+    meta,
+  }) {
+    this.id = id;
+    this.name = name;
+    this.lastName = lastName;
+    this.gender = gender;
+    this.meta = meta;
+  }
+}
+
+const genders = {
+  NOT_SPECIFIED: 0,
+  FEMALE: 1,
+  MALE: 2,
+};
+
+export default userConstants = { genders }
